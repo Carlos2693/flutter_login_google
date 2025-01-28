02180a308fa85564e377d1edc963ddc040b49bb3
Carlos Buruel <charly.ob.26@gmail.com>
2025-01-27 19:41:53 -0600

    [BOc] init commit

---
+++ lib/main.dart
@@ -0,0 +1,34 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return const MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: MainScreen(),
+    );
+  }
+}
+
+class MainScreen extends StatelessWidget {
+  const MainScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Building title'),
+      ),
+      body: const Center(
+        child: Text('Building app...'),
+      ),
+    );
+  }
+}
